5909b4cd1802d9c65b403db61715ae8cfda25e14
tanmayiependyala <106088202+tanmayiependyala@users.noreply.github.com>
2022-05-23 07:21:00 -0700

    created food file

---
+++ food.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+switch(n)
+{
+   case 1:
+        printf("Food item - Pizza\n");
+        printf("Price - Rs 239");
+        break;
+   case 2:
+        printf("Food item - Burger\n");
+        printf("Price - Rs 129");
+        break;
+   case 3:
+        printf("Food item - Pasta\n");
+        printf("Price - Rs 179");
+        break;
+   case 4:
+        printf("Food item - French Fries\n");
+        printf("Price - Rs 99");
+        break;
+   case 5:
+        printf("Food item - Sandwitch\n");
+        printf("Price - Rs 149");
+        break;
+  }
+  return 0;
+  }
